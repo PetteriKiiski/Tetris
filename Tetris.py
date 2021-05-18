34e04ae37e64baad6ea7937ae512b55426c40039
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2021-05-18 08:55:37 -0700

    School Time!

--- Tetris.py
+++ Tetris.py
@@ -10,12 +10,13 @@
 #Piece falls in place because of out of time
 #Add real pieces
 #Clears row
-import random, pygame
+import random, pygame, time
 from pygame.locals import *
 #---Piece Class---
 class Piece:
 	def __init__(self, no):
 		#XY pos's for each piece
+		self.no = no
 		self.no_co = {1:[[0, 0], [1, 0], [0, 1], [1, 1]], \
 			2:[[0, 0], [1, 0], [2, 0], [3, 0]], \
 			3:[[0, 0], [0, 1], [1, 1], [2, 1]], \
@@ -29,8 +30,21 @@ class Piece:
 		self.loc = self.no_co[no][:]
 		self.piece = pygame.image.load(self.no_img[no])
 		self.faded = pygame.image.load(self.no_fade[no])
+	#id == 0, x moves
+	#id == 1, y moves
 	def move(self, id, speed):pass
+		for i in range(4):
+			self.loc[i][id] += speed
+def RemovePiece(self, piece):
+	for co in piece.loc:
+		grid[co[1]][co[0]] = None
+def GridPiece(self, piece):
+	colorDict = {1:'O', 2:'I', 3:'L', 4:'J', 5:'S', 6:'T', 7:'Z'}
+	color = colorDict[piece.no]
+	for co in piece.loc:
+		grid[co[1]][co[0]] = color
 def Tetris():
+	global grid
 	grid = [ \
 	[None, None, None, None, None, None, None, None, None, None], \
 	[None, None, None, None, None, None, None, None, None, None], \
@@ -52,4 +66,13 @@ def Tetris():
 	[None, None, None, None, None, None, None, None, None, None], \
 	[None, None, None, None, None, None, None, None, None, None], \
 	[None, None, None, None, None, None, None, None, None, None]]
+	pieces = [Piece(random.randint(1, 7))]
+	canvas = pygame.display.set_mode((250, 500))
+	clock = pygame.time.Clock()
+	move = time.time()
+	while True:
+		clock.tick(50)
+
+		if time.time() - move >= 1:
+			pieces[-1].move(1, 1)
 Tetris()
