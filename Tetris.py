ff7c16984a37554c59a424fc0534f354487c35c8
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2021-05-18 17:02:05 -0700

    Moving pieces according to mouse

--- Tetris.py
+++ Tetris.py
@@ -5,16 +5,15 @@
 #3-100
 #4-500
 #To do list:
-#Create Grid
 #Add Imaginary pieces
-#Piece falls in place because of out of time
 #Add real pieces
 #Clears row
 import random, pygame, time, sys
 from pygame.locals import *
+pygame.init()
 #---Piece Class---
 class Piece:
-	def __init__(self, no):
+	def __init__(self, no, x):
 		#XY pos's for each piece
 		self.no = no
 		self.no_co = {1:[[0, 0], [1, 0], [0, 1], [1, 1]], \
@@ -28,42 +27,32 @@ class Piece:
 		self.no_fade = {1:"FadedYellow.png", 2:"FadedTurqoise.png", 3:"FadedBlue.png", 4:"FadedOrange.png", 5:"FadedGreen.png", 6:"FadedPurple.png", 7:"FadedRed.png"}
 		self.orig_loc = self.no_co[no]
 		self.loc = self.no_co[no][:]
+		self.set_x(x)
 		self.piece = pygame.image.load(self.no_img[no])
 		self.faded = pygame.image.load(self.no_fade[no])
 	#id == 0, x moves
 	#id == 1, y moves
+	def set_x(self, x):
+		RemovePiece(self)
+		for i in range(4):
+			self.loc[i][0] -= self.loc[0][0] - x
+		GridPiece(self)
 	def move(self, id, speed):
-#		print (self.loc)
 		temp_loc = self.loc
 		try:
 			for i in range(4):
 				temp_loc[i][id] += speed
-#				if grid[temp_loc[i][1]][temp_loc[i][0]] != None or temp_loc[i][1] == 19:
-#					print (self.loc)
-#					print ("False")
-#					return False
 		except Exception as err:
 			print (err)
 			return False
-#		temp_loc2 = temp_loc[:]
-#		try:
-#			for i in range(4):
-#				temp_loc2[i][id] += speed
-#		except Exception as err:
-#			print (err)
 		for i in range(4):
 			try:
 				if grid[temp_loc[i][1] + 1][temp_loc[i][0]] != None:
-					print (self.loc)
-					print (not grid[temp_loc[i][1]][temp_loc[i][0]] != None)
 					return False
 			except:
 				pass
 			if temp_loc[i][1] == 19:
-				print (self.loc)
-				print (not grid[temp_loc[i][1]][temp_loc[i][0]] != None)
 				return False
-		print ("Happens")
 		self.loc = temp_loc[:]
 		return True
 def RemovePiece(piece):
@@ -96,20 +85,22 @@ def Tetris():
 	[None, None, None, None, None, None, None, None, None, None], \
 	[None, None, None, None, None, None, None, None, None, None], \
 	[None, None, None, None, None, None, None, None, None, None]]
-	pieces = [Piece(random.randint(1, 7))]
+	m_pos = pygame.mouse.get_pos()
+	pieces = [Piece(random.randint(1, 7), m_pos[0] // 25)]
 	canvas = pygame.display.set_mode((250, 500))
 	pygame.display.set_caption("Tetris")
 	move = time.time()
 	GridPiece(pieces[-1])
 	while True:
 		canvas.fill((255, 255, 255))
+		m_pos = pygame.mouse.get_pos()
+		pieces[-1].set_x(m_pos[0] // 25)
 		RemovePiece(pieces[-1])
-		if time.time() - move >= 1:
+		if time.time() - move >= 0.5:
 			move = time.time()
 			if not pieces[-1].move(1, 1):
-				print (pieces[-1].loc)
 				GridPiece(pieces[-1])
-				pieces += [Piece(random.randint(1, 7))]
+				pieces += [Piece(random.randint(1, 7), m_pos[0] // 25)]
 		GridPiece(pieces[-1])
 		y = -1
 		x = -1
